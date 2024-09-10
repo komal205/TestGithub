c88eead1dde188d02dceed3aadd0119ef5198d0c
komal205 <p.komal221@gmail.com>
2024-09-10 23:52:40 +0530

    Print Statement in Multiply file

--- Multiply.java
+++ Multiply.java
@@ -1,4 +1,7 @@
 int a=9;
 int b=8;
 int c=a*b;
-System.ouy.println(Multiply c: "+c);
+System.ouy.println("Multiply c: "+c);
+System.ouy.println("Multiply a: "+a);
+System.ouy.println("Multiply b: "+b);
+
--- Substraction.java
+++ Substraction.java
@@ -3,4 +3,3 @@ int b=7;
 int c= a-b;
 System.out.println("Substraction c: "+c);
 System.out.println("int a:"+a);
-System.out.println("int b:"+b);
